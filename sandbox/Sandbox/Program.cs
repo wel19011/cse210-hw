05f11026acd6e6785a79e98fbbe0e1e057ce9814
wel19011 <wel19011@byui.edu>
2024-06-10 14:43:23 -0600

    Added a few notes about Syntax for C#

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello SB World!");
+        Console.WriteLine("This is a test of working with C# syntax");
+        Console.WriteLine("The following are examples the syntax");
+
+        int x = 10;
+        double y = 10.5624;
+        string a = "bob";
+
+        Console.WriteLine($"My integer: {x}, my double: {y}, the string: {a}");
+
+        // string response = Console.Read("Input your age: ");
     }
 }
