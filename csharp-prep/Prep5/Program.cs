911511c1b5edfff6e73bbffc494f6d43b33350fd
wel19011 <wel19011@byui.edu>
2024-06-14 10:53:22 -0600

    Wrote program to get user name and fav number and display name and the square of their number.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Jason Bourne, welcome to the program... \nthat computes the square of your favorite number.");
+        // return welcomeMessage;
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static int SquareNumber(int userNumber)
+    {
+        int numberSquared = userNumber * userNumber;
+        return numberSquared;
+    }
+
+    static void DisplayResult(string userName, int numberSquared)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {numberSquared}.");
+        // return display
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
+
     }
 }
