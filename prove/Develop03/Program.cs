c88000091b51193ef5fee09721b4c42dd801517c
wel19011 <wel19011@byui.edu>
2024-06-25 15:15:28 -0600

    Started the scripture class and added a verse in the constructor

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Welcome to the Scripture memorization program!");
+
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,13 @@
+class Scripture
+{
+    private string _verse;
+
+    public Scripture()
+    {
+        _verse = "John 3:16 For God so loved the world that He gave His only begotten Son. That whosoever believeth in Him should not perish, but  have everlasting life";
+    }
+    public string GetVerse()
+    {
+        return _verse;
+    }
+}
