7ceb5f5994107ed835d980a932283223593791ec
wel19011 <wel19011@byui.edu>
2024-06-25 15:58:33 -0600

    Added CombineParts method to Reference class

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -5,12 +5,30 @@ class Reference
     private int _verse;
     private string _reference;
 
+    public Reference()
+    {
+
+    }
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
     private string GrabReference(string verse)
     {
         // call the parse and grab the first word in the string
         Words w = new Words();
         List<string> _words = w.ParseVerse(verse);
-        _reference = _words[0];
+        _book = _words[0];
+        _chapter = int.Parse(_words[1]);
+        _verse = int.Parse(_words[2]);
+        _reference = CombineParts(_book, _chapter, _verse);
+        return _reference;
+    }
+    private string CombineParts(string book, int chapter, int verse)
+    {
+        _reference = $"{book} {chapter} {verse}";
         return _reference;
     }
 }
