4e1e997247c3a30104bb655aba5d2d8ed7d0bc2e
wel19011 <wel19011@byui.edu>
2024-06-24 19:52:44 -0600

    Wrote the Fraction class

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,45 @@
+class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 0;
+        _denominator = 0;
+    }
+    public Fraction(int top)
+    {
+        _numerator = top;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _numerator = top;
+        _denominator = bottom;
+    }
+    public void GetTop()
+    {
+        Console.WriteLine($"Numerator: {_numerator}");
+    }
+    public void SetTop(int top)
+    {
+        _numerator = top;
+    }
+    public void GetBottom()
+    {
+        Console.WriteLine($"Denominator: {_denominator}");
+    }
+    public void SetBottom(int bottom)
+    {
+        _denominator = bottom;
+    }
+    public string GetFractionString()
+    {
+        string top = _numerator.ToString();
+        return top;
+    }
+    public double GetDecimalValue()
+    {
+        return _numerator / _denominator;
+    }
+}
