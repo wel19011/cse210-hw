91a9afac12f749ae01aaf0b554b79c2b31e16efb
wel19011 <wel19011@byui.edu>
2024-07-03 15:05:34 -0600

    Began writing a disply menu function in main

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,6 +2,20 @@ using System;
 
 class Program
 {
+    static int DisplayMenu()
+    {
+        int response = 0;
+        while (response < 1 || response > 4)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("Type q to exit");
+        }   response = Console.Read();
+        return response;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
