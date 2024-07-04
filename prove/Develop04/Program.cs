903996a1974792aad33ea07ace90920ad82adb89
wel19011 <wel19011@byui.edu>
2024-07-03 19:25:52 -0600

    Added a DiplayMenu function to main and fixed bug of getting stuck in a loop

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -7,13 +7,14 @@ class Program
         int response = 0;
         while (response < 1 || response > 4)
         {
-            Console.Clear();
+            // Console.Clear();
             Console.WriteLine("Menu Options: ");
             Console.WriteLine("1. Breathing Activity");
             Console.WriteLine("2. Reflection Activity");
             Console.WriteLine("3. Listing Activity");
-            Console.WriteLine("Type q to exit");
-        }   response = Console.Read();
+            Console.WriteLine("4. Quit ");
+            response = int.Parse(Console.ReadLine());
+        }   
         return response;
     }
     static void Main(string[] args)
@@ -23,5 +24,8 @@ class Program
         Activity act1 = new Activity("Essay", "Write 10 pages about the American Revolution", 10, "Thank you for completing your assignment");
         Console.WriteLine(act1.GetActivityName());
         Console.WriteLine(act1.GetDescription());
+
+        int response = DisplayMenu();
+        Console.WriteLine(response);
     }
 }
