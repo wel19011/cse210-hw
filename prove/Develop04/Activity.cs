c9a27b91df4cd78ec681bd4e6261003cd93712e9
wel19011 <wel19011@byui.edu>
2024-07-03 20:45:48 -0600

    Wrote DisplayWelcome method of Activity, it works

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -20,17 +20,36 @@ public class Activity
     {
         return _activityName;
     }
+    public int DisplayWelcome()
+    {
+        Console.WriteLine($"Welcome to the {_activityName} Activity.");
+        Console.WriteLine($"{_description}");
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+        return _duration;
+    }
     public void Timer()
     {
         // Functions to time the activity
+        DateTime currentTime = DateTime.Now;
+        DateTime futureTime = currentTime.AddSeconds(_duration);
+
+            Console.Write("Waiting...");
+        while (DateTime.Now < futureTime)
+        {
+            Thread.Sleep(1000);
+            Console.Write(".");
+        }
+        Console.WriteLine("Finished!");
+        // if (futureTime < currentTime)
     }
     public void Animation()
     {
-        // for loop that iterates through four symbols (/-\|)
-        for (int i = 0; i < 8; i++)
+        // counts down from 8
+        for (int i = 5; i > 0; i--)
         {
             Console.Write($"{i}");
-            Thread.Sleep(500);
+            Thread.Sleep(750);
             Console.Write("\b \b");
         }
     }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -14,21 +14,46 @@ class Program
             Console.WriteLine("3. Listing Activity");
             Console.WriteLine("4. Quit ");
             response = int.Parse(Console.ReadLine());
+            if (response == 4)
+            {
+                break; 
+            }
         }   
-        return response;
+            return response;
     }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
 
-        Activity act1 = new Activity("Essay", "Write 10 pages about the American Revolution", 10, "Thank you for completing your assignment");
-        Console.WriteLine(act1.GetActivityName());
-        Console.WriteLine(act1.GetDescription());
+        // Activity act1 = new Activity("Essay", "Write 10 pages about the American Revolution", 10, "Thank you for completing your assignment");
+        // Console.WriteLine(act1.GetActivityName());
+        // Console.WriteLine(act1.GetDescription());
 
         int response = DisplayMenu();
-        Console.WriteLine(response);
+        // Console.WriteLine(response);
 
-        Activity act2 = new Activity("Sleep", "breath lots", 20, "Thank you for participating");
-        act2.Animation();
+        // Activity act2 = new Activity("Sleep", "breath lots", 5, "Thank you for participating");
+        // // act2.Animation();
+        // act2.Timer();
+
+        if (response == 1)
+        {
+            // Breating activity
+            BreathingActivity ba = new BreathingActivity("Breathing", "This activity will teach you how to breath", 0, "Thank you for participating", "prompt");
+            ba.DisplayWelcome();
+            ba.Timer();
+        }
+        else if (response == 2)
+        {
+            // Reflection activity
+        }
+        else if (response == 3)
+        {
+            // Lising Activity
+        }
+        else if (response == 4)
+        {
+            // Display goodbye message
+        }
     }
 }
