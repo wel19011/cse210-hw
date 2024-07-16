0143c082b2d70859b4f5c64b0e5617f37898648e
wel19011 <wel19011@byui.edu>
2024-07-16 15:58:30 -0600

    Added functionality to read in the file

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Console.WriteLine("Hello Bioinformatics World!");
+
+        // Console.Write("Enter the filename: ");
+        // string filename = Console.ReadLine();
+        string filename = "1fg9.pdb";
+        // string [] lines = System.IO.File.ReadAllLines(filename);
+
+        var atomLines = File.ReadLines(filename).Where(line => line.StartsWith("ATOM"));
+        int atoms = 0;
+        foreach (var line in atomLines)
+        {
+            atoms++;
+        }
+        Console.WriteLine(atoms);
     }
 }
