8ace7a3cd0be82247e517dfaba4cea5078fa5d62
wel19011 <wel19011@byui.edu>
2024-07-16 16:09:03 -0600

    updated ReadFile method in main

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -2,21 +2,19 @@ using System;
 
 class Program
 {
+    static string [] ReadFile()
+    {
+        // Console.Write("Enter the filename: ");
+        // string filename = Console.ReadLine();
+        string filename = "1fg9.pdb";
+
+        string [] atomLines = File.ReadLines(filename).Where(line => line.StartsWith("ATOM")).ToArray();
+        return atomLines;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Bioinformatics World!");
 
-        // Console.Write("Enter the filename: ");
-        // string filename = Console.ReadLine();
-        string filename = "1fg9.pdb";
-        // string [] lines = System.IO.File.ReadAllLines(filename);
 
-        var atomLines = File.ReadLines(filename).Where(line => line.StartsWith("ATOM"));
-        int atoms = 0;
-        foreach (var line in atomLines)
-        {
-            atoms++;
-        }
-        Console.WriteLine(atoms);
     }
 }
