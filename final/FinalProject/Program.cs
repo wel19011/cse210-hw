c7b25212de00f5c2b7f26acfddd8538d0afe0d4d
wel19011 <wel19011@byui.edu>
2024-07-17 15:12:26 -0600

    selected atomSerialNumber, atomName, residueName, chainIdentifier

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -19,9 +19,12 @@ class Program
         foreach (string line in atomLines)
         {
             // Console.WriteLine(line[2]);
-            string tag = line[0..4];
+            string tag = line[0..4];    // to selectthe appropriate columns, take the first parameter - 1, and leave the second parameter as it is written on this website: https://www.cgl.ucsf.edu/chimera/docs/UsersGuide/tutorials/pdbintro.html
             int atomSerialNumber = int.Parse(line[6..11]);
-            Console.WriteLine(atomSerialNumber);
+            string atomName = line[12..16];
+            string residueName = line[17..20];
+            char chainIdentifier = line[21];
+            Console.WriteLine(chainIdentifier);
         }
     }
 }
